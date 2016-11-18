2cd6d320fd9952d6843b349336397e542ff343c6
douglasjosenieves PC Escritorio Casa <douglasjosenieves@gmail.com>
2016-11-18 15:42:40 -0430

    isto
    iso

--- interprise/nav_define.php
+++ interprise/nav_define.php
@@ -1,7 +1,7 @@
 <?php  
 
 
-define('BASE_URL', 'http://localhost/erpgea2/interprise/'); 
+define('BASE_URL', 'http://localhost/gea/interprise/'); 
 /*define('BASE_URL', 'http://decototal.tk/interprise/'); */
 
 
