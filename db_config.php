388763ff0804618354eeb16c6a07ae151bc913fe
Mi casa <douglasjosenieves@gmail.com>
2017-01-17 14:01:35 -0400

    CAmbio de config

--- db_config.php
+++ db_config.php
@@ -4,7 +4,7 @@ define('DB_PASSWORD', "programador"); // db password (mention your db password h
 define('DB_DATABASE', "erp"); // database name
 define('DB_SERVER', "127.0.0.1"); // db server
  
-define('MONEDA',"Bs.");
+define('MONEDA',"Bs");
 define('IMPUESTO',12);
 define('RIF','RUC');
 define('IVA','ITBMS');
